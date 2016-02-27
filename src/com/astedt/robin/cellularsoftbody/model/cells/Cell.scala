64f976d57df5cbc9807d449c1526c969924e9d12
Robin Åstedt <robin.astedt@gmail.com>
2016-02-27 17:04:31 +0100

    Updated Cell constructor and members

--- src/com/astedt/robin/cellularsoftbody/model/cells/Cell.scala
+++ src/com/astedt/robin/cellularsoftbody/model/cells/Cell.scala
@@ -2,11 +2,24 @@ package com.astedt.robin.cellularsoftbody.model.cells
 
 import com.astedt.robin.cellularsoftbody.model.cells.phenotypes.Photosynthesis;
 
+class Velocity(var x : Double, var y : Double);
 
-object Cell {
-  def testCell = new Cell with Photosynthesis
+class Position(var x : Double, var y : Double) {
+  def incr(vel : Velocity) {
+    x += vel.x;
+    y += vel.y;
+  }
 }
 
-class Cell {
+object Cell {
+  def testCell = new Cell(new Position(0,0)) with Photosynthesis;
   
+}
+
+class Cell(private var position : Position) {
+  private var velocity = new Velocity(0,0);
+  
+  def step(dt : Double) {
+    position.incr(velocity);
+  }
 }
